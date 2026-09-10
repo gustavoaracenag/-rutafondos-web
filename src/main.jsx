fd9648592adadcaaa9235f7c7c24f39444685f67
gustavoaracenag <gustavo.aracena.g@gmail.com>
2026-09-10 11:43:52 -0300

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import RutaFondos from "./RutaFondos.jsx";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RutaFondos />
+  </React.StrictMode>
+);
